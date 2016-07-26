8be57ab24fbe7fd9c04b8c634807b6bcf37a496e
Puss In Boot <weiwenhuaming@hotmail.com>
2016-07-26 15:54:25 +0800

    update base canvas content

--- app/src/main/java/in/iheart/voicememo/util/DisplayUtil.java
+++ app/src/main/java/in/iheart/voicememo/util/DisplayUtil.java
@@ -1,6 +1,10 @@
 package in.iheart.voicememo.util;
 
 import android.content.Context;
+import android.graphics.Rect;
+import android.text.TextPaint;
+
+import java.text.DecimalFormat;
 
 /**
  * Author：weiwenhuaming on 2016/7/22 17:40
@@ -30,5 +34,33 @@ public class DisplayUtil {
 
     public static int getHeight(Context context) {
         return context.getResources().getDisplayMetrics().heightPixels;
+    }
+
+    /**
+     * 获取Text高度
+     *
+     * @param tp
+     * @return
+     */
+    public static int getTextHeight(TextPaint tp) {
+        Rect rect = new Rect();
+        tp.getTextBounds("0", 0, 1, rect);
+        return rect.height();
+    }
+
+
+    /**
+     * 根据位置获取时间(最大59:59)
+     *
+     * @param position
+     * @return
+     */
+    public static String getTimeByPosition(int position) {
+        DecimalFormat df = new DecimalFormat("00");
+        if (position < 60)
+            return "00:" + df.format(position);
+        else {
+            return df.format(position / 60) + ":" + df.format(position % 60);
+        }
     }
 }
--- app/src/main/java/in/iheart/voicememo/view/VoiceView.java
+++ app/src/main/java/in/iheart/voicememo/view/VoiceView.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.text.TextPaint;
 import android.util.AttributeSet;
 import android.view.View;
 
@@ -16,19 +17,87 @@ public class VoiceView extends View { //in.iheart.voicememo.view.VoiceView
 
     private Context mContext;
 
+    /**
+     * 蓝色时间线模式(初始化,移动,中间三种模式)
+     **/
+    private static final int INIT_MODE_TIME_LINE = 0;
+    private static final int MOVE_MODE_TIME_LINE = 1;
+    private static final int MIDDLE_MODE_TIME_LINE = 2;
+
+    /**
+     * 移动速度
+     */
+    private static final int MOVE_STEP_SPEED = 5;
+
+    /**
+     * 左声道高度
+     **/
     private float LEFT_VOICE_CHANNEL_HEIGHT = 100F;
+    /**
+     * 右声道高度
+     **/
     private float RIGHT_VOICE_CHANNEL_HEIGHT = 100F;
+    /**
+     * 时间刻度宽度
+     **/
     private float TIME_STEP_WIDTH = 20F;
+    /**
+     * 时间刻度高度
+     **/
     private float TIME_STEP_HEIGHT = 40F;
 
     private int mHeight;
     private int mWidth;
+    /**
+     * 声道刻度高度
+     **/
+    private int mDialHeight;
+    /**
+     * 声道刻度外部偏移量
+     **/
+    private int mDialOffset;
+    /**
+     * 时间刻度高度(时间指针高度)
+     **/
+    private int mTimeDialHeight;
+    /**
+     * 声道刻度字体大小
+     **/
+    private int mVoiceDialSize;
+    /**
+     * 时间刻度字体大小
+     **/
+    private int mTimeDialSize;
+
+    /**
+     * 时间线模式
+     */
+    private int mTimeLineMode = MOVE_MODE_TIME_LINE;
+
+
+    private boolean isInitFinish;
+
+    /**
+     * 时间线步进
+     */
+    private int mTimeLineStepOffset;
+    /**
+     * 时间刻度步进
+     */
+    private int mTimeDialStepOffset;
+
+    /**
+     * 时间记录
+     */
+    private int mTimeHistory;
+
 
     private Paint mBackgroundPaint;
     private Paint mRedPaint;
     private Paint mGrayPaint;
     private Paint mBluePaint;
     private Paint mWhitePaint;
+    private TextPaint mTextPaint;
 
 
     public VoiceView(Context context) {
@@ -62,6 +131,13 @@ public class VoiceView extends View { //in.iheart.voicememo.view.VoiceView
         TIME_STEP_WIDTH = DisplayUtil.dip2pxF(mContext, TIME_STEP_WIDTH);
         TIME_STEP_HEIGHT = DisplayUtil.dip2pxF(mContext, TIME_STEP_HEIGHT);
 
+        mDialOffset = DisplayUtil.dip2px(mContext, 10);
+        mDialHeight = (int) ((LEFT_VOICE_CHANNEL_HEIGHT - mDialOffset) / 7);
+
+        mVoiceDialSize = DisplayUtil.dip2px(mContext, 8);
+        mTimeDialSize = DisplayUtil.dip2px(mContext, 12);
+        mTimeDialHeight = DisplayUtil.dip2px(mContext, 5);
+
         mBackgroundPaint = new Paint();
         mBackgroundPaint.setAntiAlias(true);
         mBackgroundPaint.setColor(Color.BLACK);
@@ -73,16 +149,19 @@ public class VoiceView extends View { //in.iheart.voicememo.view.VoiceView
         mGrayPaint = new Paint();
         mGrayPaint.setAntiAlias(true);
         mGrayPaint.setColor(Color.GRAY);
+        mGrayPaint.setStrokeWidth(DisplayUtil.dip2px(mContext, 1));
 
         mBluePaint = new Paint();
         mBluePaint.setAntiAlias(true);
-        mBluePaint.setColor(Color.BLUE);
+        mBluePaint.setColor(Color.parseColor("#0074ED"));
+        mBluePaint.setStrokeWidth(DisplayUtil.dip2px(mContext, 1));
 
         mWhitePaint = new Paint();
         mWhitePaint.setAntiAlias(true);
         mWhitePaint.setColor(Color.WHITE);
 
-
+        mTextPaint = new TextPaint();
+        mTextPaint.setAntiAlias(true);
     }
 
 
@@ -91,16 +170,141 @@ public class VoiceView extends View { //in.iheart.voicememo.view.VoiceView
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
 
         setMeasuredDimension(mWidth, mHeight);
+
+
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
         drawGrayHoriLine(canvas);
+        drawVoiceChannelDial(canvas);
 
+        drawTimeDial(canvas);
+        drawCurrentTimeLine(canvas);
+        drawTimeByDial(canvas);
+
+        invalidate();
     }
 
+    /**
+     * 绘制时间刻度值
+     *
+     * @param canvas
+     */
+    private void drawTimeByDial(Canvas canvas) {
+        mTextPaint.setColor(Color.WHITE);
+        mTextPaint.setTextSize(mTimeDialSize);
+        float height = DisplayUtil.getTextHeight(mTextPaint);
+        //文字偏移量:起始偏移1.5的TIME_STEP_WIDTH,文字偏移量是低时间指针
+        float startOffset = TIME_STEP_WIDTH * 1.5F + mTimeDialHeight - mTimeDialStepOffset;
+        //每次绘制屏幕宽度容纳的数量加上5
+        int times = (int) (mWidth / TIME_STEP_WIDTH + 5);
+        canvas.save();
+        for (int i = 0; i < times; i++) {
+            if (i % 4 == 0) {
+                int temp = mTimeHistory + i / 4;
+                String text = DisplayUtil.getTimeByPosition(temp);
+                canvas.drawText(text, TIME_STEP_WIDTH * i + startOffset, mTimeDialHeight * 4 + height, mTextPaint);
+            }
+        }
+        canvas.restore();
+    }
+
+    /**
+     * 绘制当前时间点上的标记线
+     *
+     * @param canvas
+     */
+    private void drawCurrentTimeLine(Canvas canvas) {
+        float startOffset = TIME_STEP_WIDTH * 1.5F;
+        if (mTimeLineMode == MIDDLE_MODE_TIME_LINE) {
+            startOffset = mWidth / 2;
+            mTimeDialStepOffset += MOVE_STEP_SPEED;
+            if (mTimeDialStepOffset >= TIME_STEP_WIDTH * 4) {
+                mTimeDialStepOffset = 0;
+                mTimeHistory++;
+            }
+        }
+        if (mTimeLineMode == MOVE_MODE_TIME_LINE) {
+            startOffset += (mTimeLineStepOffset += MOVE_STEP_SPEED);
+            if ((int) (startOffset + 0.5F) >= mWidth / 2) {
+                //改变时间线模式,并同步使时间刻度动起来
+                mTimeLineMode = MIDDLE_MODE_TIME_LINE;
+            }
+        }
+        canvas.save();
+        //绘制中间线
+        canvas.drawLine(startOffset, TIME_STEP_HEIGHT, startOffset, TIME_STEP_HEIGHT + LEFT_VOICE_CHANNEL_HEIGHT + RIGHT_VOICE_CHANNEL_HEIGHT, mBluePaint);
+        //绘制上下两端圆形
+        float radius = mTimeDialHeight * 1.5F / 2;
+        canvas.drawCircle(startOffset, TIME_STEP_HEIGHT - radius, radius, mBluePaint);
+        canvas.drawCircle(startOffset, TIME_STEP_HEIGHT + LEFT_VOICE_CHANNEL_HEIGHT + RIGHT_VOICE_CHANNEL_HEIGHT + radius, radius, mBluePaint);
+        canvas.restore();
+    }
+
+    /**
+     * 绘制时间刻度
+     *
+     * @param canvas
+     */
+    private void drawTimeDial(Canvas canvas) {
+        mGrayPaint.setAlpha(200);
+        float offsetX = -(TIME_STEP_WIDTH * 2.5F + mTimeDialStepOffset);
+        int times = (int) (mWidth / TIME_STEP_WIDTH + 10);
+        canvas.save();
+        for (int i = 0; i < times; i++) {
+            float startX = TIME_STEP_WIDTH * i + offsetX;
+            float startY = TIME_STEP_HEIGHT - mTimeDialHeight;
+            float stopX = startX;
+            float stopY = TIME_STEP_HEIGHT;
+            if (i % 4 == 0)
+                startY = TIME_STEP_HEIGHT - mTimeDialHeight * 4;
+            canvas.drawLine(startX, startY, stopX, stopY, mGrayPaint);
+        }
+        canvas.restore();
+    }
+
+    /**
+     * 绘制声道音量刻度
+     *
+     * @param canvas
+     */
+    private void drawVoiceChannelDial(Canvas canvas) {
+        mTextPaint.setColor(Color.GRAY);
+        mTextPaint.setTextSize(mVoiceDialSize);
+
+        int height = mDialHeight - DisplayUtil.getTextHeight(mTextPaint);
+
+        canvas.save();
+        for (int i = 0; i < 7; i++) {
+            int temp = i;
+            if (temp == 6)
+                temp = 10;
+            if (temp == 5)
+                temp = 7;
+            if (temp == 4)
+                temp = 5;
+            String text = temp - temp * 2 + "";
+            //以声道中间横线为界,向上下两边绘制刻度
+            float leftY = TIME_STEP_HEIGHT + LEFT_VOICE_CHANNEL_HEIGHT - mDialHeight * (7 - i) + height;
+            float rightY = TIME_STEP_HEIGHT + LEFT_VOICE_CHANNEL_HEIGHT + mDialHeight * (7 - i);
+
+            float x = mWidth - mTextPaint.measureText(text) - TIME_STEP_WIDTH / 3;
+
+            canvas.drawText(text, x, leftY, mTextPaint);
+            canvas.drawText(text, x, rightY, mTextPaint);
+        }
+        canvas.restore();
+    }
+
+    /**
+     * 绘制声道分割线
+     *
+     * @param canvas
+     */
     private void drawGrayHoriLine(Canvas canvas) {
+        mGrayPaint.setAlpha(255);
         canvas.save();
         //绘制第一条横线
         canvas.drawLine(0, TIME_STEP_HEIGHT, mWidth, TIME_STEP_HEIGHT, mGrayPaint);
